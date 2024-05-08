93e76fefe2f26dd0d0e6951d602984339c474a03
Moderocky <admin@moderocky.com>
2024-05-08 13:04:41 +0100

    Deprecate and replace Setter with Java's Consumer. (#6667)
    * Make Setter a Consumer.
    
    * Remove Setter from Option.
    
    * Remove setter from entry validator.
    
    * Remove setter from enum entry validator.
    
    * Remove setter from parsed entry validator.
    
    * Remove setter from parsed section validator.

--- src/main/java/ch/njol/skript/config/validate/ParsedEntryValidator.java
+++ src/main/java/ch/njol/skript/config/validate/ParsedEntryValidator.java
@@ -22,23 +22,24 @@ import ch.njol.skript.classes.Parser;
 import ch.njol.skript.config.EntryNode;
 import ch.njol.skript.config.Node;
 import ch.njol.skript.lang.ParseContext;
-import ch.njol.util.Setter;
+
+import java.util.function.Consumer;
 
 /**
  * @author Peter Güttinger
  */
 public class ParsedEntryValidator<T> extends EntryValidator {
-	
+
 	private final Parser<? extends T> parser;
-	private final Setter<T> setter;
-	
-	public ParsedEntryValidator(final Parser<? extends T> parser, final Setter<T> setter) {
+	private final Consumer<T> setter;
+
+	public ParsedEntryValidator(final Parser<? extends T> parser, final Consumer<T> setter) {
 		assert parser != null;
 		assert setter != null;
 		this.parser = parser;
 		this.setter = setter;
 	}
-	
+
 	@Override
 	public boolean validate(final Node node) {
 		if (!super.validate(node))
@@ -46,8 +47,8 @@ public class ParsedEntryValidator<T> extends EntryValidator {
 		final T t = parser.parse(((EntryNode) node).getValue(), ParseContext.CONFIG);
 		if (t == null)
 			return false;
-		setter.set(t);
+		setter.accept(t);
 		return true;
 	}
-	
+
 }
