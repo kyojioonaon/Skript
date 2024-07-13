093fa835c8247403194630eb1c1df68129655cae
sovdee <10354869+sovdeeth@users.noreply.github.com>
2024-07-13 13:34:20 -0700

    force 'rotation' in EQAA, fix classdef error, add itemtypes to block display entitydata pattern, add tests for display entitydata
    Not sure wh

--- src/main/java/org/skriptlang/skript/elements/expressions/ExprQuaternionAxisAngle.java
+++ src/main/java/org/skriptlang/skript/elements/expressions/ExprQuaternionAxisAngle.java
@@ -16,25 +16,28 @@ import org.bukkit.event.Event;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Nullable;
 import org.joml.AxisAngle4f;
+import org.joml.Math;
 import org.joml.Quaternionf;
 
-@Name("Quaternion Axis/Angle of Rotation")
+@Name("Rotation Axis/Angle")
 @Description({
-	"Returns the axis or angle that a quaternion will rotate with.",
+	"Returns the axis or angle that a quaternion will rotate by/around.",
 	"All quaternions can be represented by a rotation of some amount around some axis, so this expression provides " +
 	"the ability to get that angle/axis."
 })
 @Examples({
 	"set {_quaternion} to axisAngle(45, vector(1, 2, 3))",
-	"send axis of {_quaternion} # 1, 2, 3",
-	"send angle of {_quaternion} # 45"
+	"send rotation axis of {_quaternion} # 1, 2, 3",
+	"send rotation angle of {_quaternion} # 45",
+	"set rotation angle of {_quaternion} to 135",
+	"set rotation axis of {_quaternion} to vector(0, 1, 0)"
 })
 @Since("INSERT VERSION")
 public class ExprQuaternionAxisAngle extends SimplePropertyExpression<Quaternionf, Object> {
 
 	static {
 		if (Skript.isRunningMinecraft(1, 19, 4))
-			register(ExprQuaternionAxisAngle.class, Object.class, "[rotation] (angle|:axis)", "quaternions");
+			register(ExprQuaternionAxisAngle.class, Object.class, "rotation (angle|:axis)", "quaternions");
 	}
 
 	boolean isAxis;
@@ -47,7 +50,8 @@ public class ExprQuaternionAxisAngle extends SimplePropertyExpression<Quaternion
 
 	@Override
 	public @Nullable Object convert(Quaternionf from) {
-		AxisAngle4f axisAngle = new AxisAngle4f(from);
+		AxisAngle4f axisAngle = new AxisAngle4f();
+		setFromQuaternion(axisAngle, from);
 		if (isAxis)
 			return new Vector(axisAngle.x, axisAngle.y, axisAngle.z);
 		return (float) (axisAngle.angle * 180 / Math.PI);
@@ -72,14 +76,14 @@ public class ExprQuaternionAxisAngle extends SimplePropertyExpression<Quaternion
 		AxisAngle4f axisAngle = new AxisAngle4f();
 		if (isAxis && delta[0] instanceof Vector vector) {
 			for (Quaternionf quaternion : quaternions) {
-		 		axisAngle.set(quaternion);
+				setFromQuaternion(axisAngle, quaternion);
 				axisAngle.set(axisAngle.angle, (float) vector.getX(), (float) vector.getY(), (float) vector.getZ());
 				quaternion.set(axisAngle);
 			}
 		} else if (delta[0] instanceof Number number) {
 			float angle = (float) (number.floatValue() / 180 * Math.PI);
 			for (Quaternionf quaternion : quaternions) {
-				axisAngle.set(quaternion);
+				setFromQuaternion(axisAngle, quaternion);
 				axisAngle.set(angle, axisAngle.x, axisAngle.y, axisAngle.z);
 				quaternion.set(axisAngle);
 			}
@@ -95,6 +99,23 @@ public class ExprQuaternionAxisAngle extends SimplePropertyExpression<Quaternion
 	@Override
 	public Class<?> getReturnType() {
 		return isAxis ? Vector.class : Float.class;
+	}
+
+	// avoid ClassNotFound exception on <1.19
+	// TODO: remove after legacy tests removed
+	private static void setFromQuaternion(AxisAngle4f axisAngle4f, Quaternionf quaternionf) {
+		float acos = Math.safeAcos(quaternionf.w());
+		float invSqrt = Math.invsqrt(1.0f - quaternionf.w() * quaternionf.w());
+		if (Float.isInfinite(invSqrt)) {
+			axisAngle4f.x = 0.0f;
+			axisAngle4f.y = 0.0f;
+			axisAngle4f.z = 1.0f;
+		} else {
+			axisAngle4f.x = quaternionf.x() * invSqrt;
+			axisAngle4f.y = quaternionf.y() * invSqrt;
+			axisAngle4f.z = quaternionf.z() * invSqrt;
+		}
+		axisAngle4f.angle = acos + acos;
 	}
 
 }
